e2cceb69fb857f99d426b26b9887a6c139792b4a
OttimonieS <137037111+OttimonieS@users.noreply.github.com>
2024-10-22 11:44:02 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/githubtest/MainActivity.kt
+++ app/src/main/java/com/example/githubtest/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             GithubtestTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "tolodon",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
